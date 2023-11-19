66c8409ff9f1669a88509443578d21fa3832cff7
Shelby Rothman <shelby.rothman@aderant.com>
2023-11-19 12:56:31 -0800

    db calls set

--- src/data/api/MovieActorStore.ts
+++ src/data/api/MovieActorStore.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { config } from '../../config';
-import { BaconFeature, BaconActor } from '../../types';
+import { BaconFeature, BaconActor, BaconMovie } from '../../types';
 
 
 const apiKey = config.TMDB_API_KEY.v3;
@@ -25,6 +25,7 @@ export class MovieActorStore {
         this.url_suffix = urlSuffix;
     }
 
+    static init = () => new MovieActorStore();
 
     /**
      * 
@@ -35,11 +36,13 @@ export class MovieActorStore {
         const url = `${this.api_base}${this.url_prefix}${title}${this.url_suffix}`;
         const response = await fetch(url);
         const data = await response.json() as { results: any[] };
-        // TODO: handling it not being the top one
         if (data && data.results && data.results.length > 0) {
             // info: likely it is the top one
             const firstFeature = data.results[ 0 ];
-            const movieObject = this.convertToBaconFeature(firstFeature);
+            const movieObject: BaconFeature = {
+                id: firstFeature.id,
+                title: firstFeature.original_title || firstFeature.title
+            };
             return movieObject;
         } else {
             throw new Error('no movie found');
@@ -58,47 +61,38 @@ export class MovieActorStore {
         const data = await response.json() as { cast: any[] };
         if (data && data.cast && data.cast.length > 0) {
             const actors = data.cast.map((actor) => this.convertToBaconActor(actor));
-            // console.log(actors)
             return actors;
         } else {
             throw new Error('invalid movie ID');
         }
     };
 
-
-
     /**
-     * @method getActorsByName
-     * @param name - the name of the actor to search for
-     * @returns {BaconActor} the actor data
-     */
-    // async getActorByName(name: string) {
-    // yea we shouldnt need this bc the actors id is enough to identify them.
-    // };
-
-
-    /**
-     * @method getFeaturesByActorId
-     * gets a list of every feature an actor has been in
+     * @method getMoviesByActorId
+     * gets a list of every movie feature an actor has been in
      * @returns 
      */
-    async getFeaturesByActorId(id: string) { 
-        // movie or combined_credits
-        const url = `${this.api_base}/person/${id}/combined_credits?api_key=${this.api_key}`;
+    async getMoviesByActorId(id: string): Promise<BaconFeature[]> {
+        const url = `${this.api_base}/person/${id}/movie_credits?api_key=${this.api_key}`;
         const response = await fetch(url);
-        const data = await response.json();
-        console.log(data)
+        const data = await response.json() as { cast: any[] };
+        if (data && data.cast && data.cast.length > 0) {
+            const movies = data.cast.map((movie) => this.convertToBaconFeature(movie));
+
+            console.log(movies)
+            return movies;
+        } else {
+            throw new Error('invalid actor ID');
+        }
     };
 
-    static init() {
-        return new MovieActorStore();
-    }
 
     /** converts response to @type {BaconFeature} */
     private convertToBaconFeature(data: any): BaconFeature {
         const movieObject: BaconFeature = {
             id: data.id,
             title: data.original_title || data.title,
+            characterName: data.character || 'unknown',
         };
         return movieObject;
     }
--- tests/api/MovieActorStore.test.ts
+++ tests/api/MovieActorStore.test.ts
@@ -3,6 +3,10 @@ import { MovieActorStore } from '../../src/data/api/MovieActorStore';
 
 // TODO: mock the fetch call...
 
+// INFO: fo now i am using this pseudoly to test the api calls but before i have api calls just male thing the logic so just want to test logicv...
+
+// .. wontt have this in git eventualy
+
 const groundhogResponse = {
     id: 137,
     title: 'Groundhog Day',
@@ -13,39 +17,19 @@ const groundhogCast = [
     { id: 1533, name: 'Andie MacDowell', characterName: 'Rita Hanson' },
     { id: 1534, name: 'Chris Elliott', characterName: 'Larry' },
     { id: 537, name: 'Stephen Tobolowsky', characterName: 'Ned Ryerson' },
-    {
-        id: 1535,
-        name: 'Brian Doyle-Murray',
-        characterName: 'Buster Green'
-    },
+    { id: 1535, name: 'Brian Doyle-Murray', characterName: 'Buster Green' },
     { id: 1536, name: 'Marita Geraghty', characterName: 'Nancy Taylor' },
     { id: 1537, name: 'Angela Paton', characterName: 'Mrs. Lancaster' },
     { id: 1538, name: 'Rick Ducommun', characterName: 'Gus' },
     { id: 1539, name: 'Rick Overton', characterName: 'Ralph' },
     { id: 1540, name: 'Robin Duke', characterName: 'Doris the Waitress' },
     { id: 1541, name: 'Carol Bivins', characterName: 'Anchorwoman' },
-    {
-        id: 1542,
-        name: 'Willie Garson',
-        characterName: "Phil's Assistant Kenny"
-    },
-    {
-        id: 236327,
-        name: 'Ken Hudson Campbell',
-        characterName: 'Man in Hallway'
-    },
+    { id: 1542, name: 'Willie Garson', characterName: "Phil's Assistant Kenny" },
+    { id: 236327, name: 'Ken Hudson Campbell', characterName: 'Man in Hallway' },
     { id: 1318541, name: 'Les Podewell', characterName: 'Old Man' },
-    {
-        id: 1674112,
-        name: 'Rod Sell',
-        characterName: 'Groundhog Official'
-    },
+    { id: 1674112, name: 'Rod Sell', characterName: 'Groundhog Official' },
     { id: 86571, name: 'Tom Milanovich', characterName: 'State Trooper' },
-    {
-        id: 1659415,
-        name: 'John M. Watson Sr.',
-        characterName: 'Bartender'
-    },
+    { id: 1659415, name: 'John M. Watson Sr.', characterName: 'Bartender' },
     { id: 36093, name: 'Peggy Roeder', characterName: 'Piano Teacher' },
     { id: 1524, name: 'Harold Ramis', characterName: 'Neurologist' },
     { id: 74487, name: 'David Pasquesi', characterName: 'Psychiatrist' },
@@ -184,11 +168,12 @@ describe('MovieActorStore', () => {
     });
 
 
-    describe('getFeaturesByActorId', () => {
+    describe('getMoviesByActorId', () => {
         it('should log to console', async () => {
             let movieStore = MovieActorStore.init();
-            let response = await movieStore.getFeaturesByActorId('1532');
-            // expect(response).toEqual(groundhogCast);
+            let response = await movieStore.getMoviesByActorId('1532');
+            expect(response.some((movie) => movie.title === 'Lost in Translation')).toBe(true);
+            expect(response.some((movie) => movie.title === 'Broken Flowers')).toBe(true);
         });
     });
 });
