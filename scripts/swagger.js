14e8f4f51b70feb5f35dc672489c9d5bf82d7b55
Shelby Rothman <shelby.rothman@aderant.com>
2023-11-19 13:47:44 -0800

    schema includes in gen script

--- scripts/swagger.js
+++ scripts/swagger.js
@@ -16,7 +16,45 @@ const doc = {
             "url": "http://localhost:4020/api",
             "description": "Local Development"
         },
-    ]
+    ],
+    components: {
+        schemas: {
+            BaconFeature: {
+                id: 1223,
+                title: 'The Wizard of Oz',
+                characterName: 'The Wicked Witch of the West',
+            },
+            BaconFeatureList: {
+                id: 33445,
+                features: [
+                    { id: 6543, title: 'The Big Lebowski', characterName: 'The Dude' },
+                    { id: 2222, title: 'Reservoir Dogs', characterName: 'Mr. Pink' },
+                    { id: 3333, title: 'The Wizard of Oz', characterName: 'The Wicked Witch of the West' },
+                    { id: 789, title: 'Another Movie Title', characterName: 'Another Character Name', },
+                ]
+            },
+            BaconActor: {
+                id: '1',
+                name: 'John Doe',
+                characterName: 'Character Name',
+            },
+            BaconMovie: {
+                id: 456,
+                title: 'Movie Title',
+            },
+            BaconActorList: {
+                id: '3',
+                actors: [
+                    { id: 422, name: 'Another Actor', characterName: 'Another Character', },
+                    { id: 123, name: 'Gwyneth Paltrow', characterName: 'Pepper Pots', },
+                    { id: 456, name: 'Jane Doe', characterName: 'Charlie Brock', },
+                ],
+            },
+
+        },
+    },
+
+
 
     // FUTURE: would eventually want security around the calls if if its a paid thing and they use that key and cross check and yea from here but for now during dev just opwen
 };
