21f1fac15d5a077b9b057f7ca4f33b64e6fc248a
Shelby Rothman <shelby.rothman@aderant.com>
2023-12-19 10:19:18 -0800

    fix: moved json file into distro with initializer

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        url: "../docs/api/openapi-bacon.json",
+        url: "openapi-bacon.json",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
